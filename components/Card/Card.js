e09a5595d062edcf0c3c396b58ebe1bea73fd855
Lohit244 <lohit244@gmail.com>
2022-04-01 00:08:28 +0530

    Adds to teams and home page

---
+++ components/Card/Card.js
@@ -0,0 +1,20 @@
+import styles from "./Card.module.css"
+import Image from "next/image"
+import testImage from "../../media/sample.jpg"
+export default function Card({heading, content, author, date, image}) {
+  return(
+    <div className={styles.card}>
+      <Image src={testImage} alt="news-image"/>
+      <h1 className={styles.heading}>
+        {heading}
+      </h1>
+      <div className={styles.meta}>
+      -<b>{author}</b> <i>at</i> <b>{date}</b>
+      </div>
+      <br/>
+      <div className={styles.content}>
+      {content}
+      </div>
+    </div>
+  )
+}
