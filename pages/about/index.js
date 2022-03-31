5c7a07cc792c48bc018c389e596309fed8c9bd14
Lohit244 <lohit244@gmail.com>
2022-03-31 22:59:36 +0530

    adds to about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,23 @@
 import Image from "next/image"
 import logo from "../../media/napslogo.png"
+import { useInView } from "react-intersection-observer";
 import aboutStyles from "./about.module.css";
+
+
+
 export default function AboutUs(){
+    const animationOptions = {
+        threshold: 1
+    }
+    const [ref1, inView1] = useInView(animationOptions)
+    const [ref2, inView2] = useInView(animationOptions)
+    const [ref3, inView3] = useInView(animationOptions)
+    const [ref4, inView4] = useInView(animationOptions)
+    const [ref5, inView5] = useInView(animationOptions)
+    const [ref6, inView6] = useInView(animationOptions)
+    const [ref7, inView7] = useInView(animationOptions)
+    const [ref8, inView8] = useInView(animationOptions)
+    
     return(
         <div className={aboutStyles.aboutCtn}>
             <div className={aboutStyles.about}>
@@ -29,6 +45,112 @@ export default function AboutUs(){
             ullamcorper a lacus vestibulum. Metus aliquam eleifend mi in nulla posuere sollicitudin. Euismod quis viverra nibh cras pulvinar mattis. Egestas diam in arcu cursus euismod quis viverra nibh cras.
             Praesent elementum facilisis leo vel fringilla est ullamcorper. Dignissim suspendisse in est ante in nibh mauris cursus. 
             </div>
+            <div className={aboutStyles.activities}>
+                <h1 className={aboutStyles.activitiesHeading}>What we do</h1>
+                <div ref={ref1} className={aboutStyles.card}>
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView1?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
+
+                <div ref={ref2} className={aboutStyles.card} >
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView2?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+                <div ref={ref3} className={aboutStyles.card}>
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView3?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
+
+                <div ref={ref4} className={aboutStyles.card}>
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView4?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
+
+                <div ref={ref5} className={aboutStyles.card}>
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView5?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
+
+                <div ref={ref6} className={aboutStyles.card}>
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView6?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
+
+                <div ref={ref7} className={aboutStyles.card}>
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView7?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
+
+                <div ref={ref8} className={aboutStyles.card}>
+                    <div className={aboutStyles.cardBar}>
+                    </div>
+                    <div className={aboutStyles.cardContent} style={inView8?{scale: "1.1"}: {scale: "1"}}>
+                    <div className={aboutStyles.cardHeading}>
+                        Nisi officia sit eiusmod ad velit laboris ex ea pariatur irure exercitation Lorem.
+                    </div>
+                    <div className={aboutStyles.cardText}>
+                        Do reprehenderit laborum cillum ad veniam do anim commodo culpa. Ullamco nisi veniam aute magna enim et. Eu dolore dolore Lorem cillum ad dolore sit officia. Laboris aliquip anim labore dolor est in ex laborum ut laborum nostrud do ut nostrud.
+                    </div>
+                    </div>
+                </div>
         </div>
     ); 
 }
