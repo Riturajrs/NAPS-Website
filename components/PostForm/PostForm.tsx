50e69b4e53ea05022390fada8054b91ce4ea5598
Lohit244 <lohit244@gmail.com>
2022-05-27 22:23:40 +0530

    working on the newpost form

--- components/PostForm/PostForm.tsx
+++ components/PostForm/PostForm.tsx
@@ -1,9 +1,9 @@
 import styles from "./PostForm.module.css"
-import { useState } from "react"
+import { FormEventHandler, useState } from "react"
 import Image from "next/image";
 import { GetStaticProps } from "next/types"
 
-
+const categories = ["Editorial", "Media Report"]
 export default function PostForm({data}){
   // ----------------------------------------------- state variables
   const [title, setTitle] = useState("");
@@ -13,15 +13,18 @@ export default function PostForm({data}){
   const [content, setContent] = useState("");
   const [category, setCategory] = useState("");
   const [summary, setSummary] = useState("");
-  // ----------------------------------------------- Change functions
+  //  Change functions
   const changeTitle = (e)=>{
     setTitle(e.target.value);
   }
   const changeAuthor = (e)=>{
     setAuthor(e.target.value);
   }
+  const changeCategory = (e)=>{
+    setCategory(e.target.value);
+  }
 
-  // ----------------------------------------------- upload image 
+  //  Upload image 
   async function uploadImage(e){
     const file = e.target.files[0];
     const fd =  new FormData();
@@ -34,9 +37,54 @@ export default function PostForm({data}){
     const data = await res.json();
     setThumbnail(data.data.URL)
   }
+  //  Submit Handler
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    // Invalid Data Handling -
+    if(title==""){
+      alert("Check The Title")
+      return;
+    }
+    if(thumbnail==""){
+      alert("Thumbnail not uploaded")
+      return;
+    }
+    if(author==""){
+      alert("Check The Author")
+      return;
+    }
+    if(category==""){
+      alert("Category not submitted")
+      return;
+    }
+    if(tags==[]){
+      alert("Check The Tags")
+      return;
+    }
+    if(content==""){
+      alert("Check The Content")
+      return;
+    }
+    if(summary==""){
+      alert("Summary not submitted")
+      return;
+    }
+    // handling
+    const dataToSubmit = {
+      title: title,
+      author: author,
+      tags: tags,
+      thumbnail: thumbnail,
+      content: content,
+      category: category,
+      summary: summary
+    }
+    console.log(dataToSubmit)
+  }
   return (
     <div className={styles.PostForm}>
-    <form>
+    <form className={styles.form} onSubmit={handleSubmit}>
+    <label htmlFor="title">Title</label>
     <input type="text" name="title" onChange={changeTitle} placeholder="Title" value={title}/>
     <div className={styles.imageContainer}>
       <Image src={thumbnail==""?"/default.png":thumbnail} layout="fill" alt="Thumbnail"/>
@@ -44,6 +92,7 @@ export default function PostForm({data}){
     <label htmlFor="images">Thumbnail: </label>
     <input type="file" name="images" onChange={uploadImage}></input>
     <br/>
+    <label>Author</label>
     <select value={author} onChange={changeAuthor}>
       <option></option>
       {data&&data.map((curauthor)=>{
@@ -54,6 +103,12 @@ export default function PostForm({data}){
           )
       })}
     </select>
+    <label>Category</label>
+    <select value={category} onChange={changeCategory}>
+      <option></option>
+      {categories.map(cat=><option key={cat}>{cat}</option>)}
+    </select>
+
     </form>
     </div>
   )
