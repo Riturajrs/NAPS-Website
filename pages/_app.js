ed3f88646a22220979461e92da0da1c12348e7fa
Lohit244 <lohit244@gmail.com>
2022-06-08 18:02:52 +0530

    forms more consistent

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Header from '../components/Header/Header'
 import Footer from "../components/Footer/Footer"
+import NextNProgress from 'nextjs-progressbar'
 import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }) {
 
   return <>
+      <NextNProgress
+      height={6}
+      />
       <Header></Header>
       <Component {...pageProps} />
       <Footer></Footer>
