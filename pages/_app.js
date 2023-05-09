99ee0d4a96445d4520e849ff94bca1ded99d0af3
lohit244 <lohit244@gmail.com>
2023-05-09 10:46:03 +0530

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,20 @@
 import Header from '../components/Header/Header'
 import Footer from "../components/Footer/Footer"
 import NextNProgress from 'nextjs-progressbar'
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }) {
 
   return <>
-      <NextNProgress
+    <NextNProgress
       height={6}
-      />
-      <Header></Header>
-      <Component {...pageProps} />
-      <Footer></Footer>
+    />
+    <Header></Header>
+    <Component {...pageProps} />
+    <Footer></Footer>
+    <Analytics />
   </>
 }
 
